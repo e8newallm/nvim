977e0c698201cc908f41f21a16bb12be039f7a90
Matthew Newall <matthewnewall@hotmail.com>
2025-11-24 10:13:22 +0000

    Remove cmakelang from ensure_installed

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,7 +1,7 @@
 return {
     "mason-org/mason-lspconfig.nvim",
     opts = {
-		ensure_installed = {"clangd", "clang-format", "cmakelang", "lua_ls"},
+		ensure_installed = {"clangd", "clang-format", "lua_ls"},
 	},
     dependencies = {
         { "mason-org/mason.nvim", opts = {} },
