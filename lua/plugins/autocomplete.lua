6fdd5a96936d19665a89da690587dab18ed294e5
Matthew Newall <matthewnewall@hotmail.com>
2025-11-30 02:41:13 +0000

    Fix non-functional autocomplete

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -2,26 +2,21 @@ return {
 	{
 		"hrsh7th/nvim-cmp",
 		dependencies = {
+			"neovim/nvim-lspconfig",
 			"hrsh7th/cmp-nvim-lsp",
-			"hrsh7th/cmp-nvim-lua",
 			"hrsh7th/cmp-buffer",
 			"hrsh7th/cmp-path",
-			"rafamadriz/friendly-snippets",
-
-			"L3MON4D3/LuaSnip",
-			"saadparwaiz1/cmp_luasnip"
+			"hrsh7th/cmp-cmdline",
+			"hrsh7th/nvim-cmp"
 		},
 		config = function()
 			local cmp = require("cmp")
-			local luasnip = require("luasnip")
-
-			require("luasnip/loaders/from_vscode").lazy_load()
 
 			local kind_icons = {
 				Text = "󰦨",
-				Method = "",
-				Function = "",
-				Constructor = "",
+				Method = "󰊕",
+				Function = "󰊕",
+				Constructor = "󰊕",
 				Field = "",
 				Variable = "",
 				Class = "",
@@ -48,57 +43,41 @@ return {
 			cmp.setup({
 				snippet = {
 					expand = function(args)
-						luasnip.lsp_expand(args.body)
+						vim.snippet.expand(args.body)
 					end,
 				},
+
+				formatting = {
+					fields = { "kind", "abbr", "menu" },
+					format = function(entry, vim_item)
+						vim_item.menu = string.format('%s', vim_item.kind)
+						vim_item.kind = string.format('%s', kind_icons[vim_item.kind])
+						return vim_item
+					end,
+				},
+
+				window = {
+					completion = {
+						col_offset = -3,
+						side_padding = 1,
+					},
+				},
+
 				mapping = cmp.mapping.preset.insert({
 					["<C-k>"] = cmp.mapping.select_prev_item(),
 					["<C-j>"] = cmp.mapping.select_next_item(),
 					["<C-d>"] = cmp.mapping.scroll_docs(-4),
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
-					["<CR>"] = cmp.mapping.confirm({
-						behavior = cmp.ConfirmBehavior.Replace,
-						select = false,
-					}),
-					["<Tab>"] = cmp.mapping(function(fallback)
-						if cmp.visible() then
-							cmp.select_next_item()
-						elseif luasnip.expand_or_jumpable() then
-							luasnip.expand_or_jump()
-						else
-							fallback()
-						end
-					end, { "i", "s" }),
-					["<S-Tab>"] = cmp.mapping(function(fallback)
-						if cmp.visible() then
-							cmp.select_prev_item()
-						elseif luasnip.jumpable(-1) then
-							luasnip.jump(-1)
-						else
-							fallback()
-						end
-					end, { "i", "s" }),
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.abort(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
 				}),
-				formatting = {
-					fields = { "kind", "abbr", "menu" },
-					format = function(entry, vim_item)
-						-- Kind icons
-						vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
-						vim_item.menu = ({
-							nvim_lsp = "[LSP]",
-							luasnip = "[Snippet]",
-							buffer = "[Buffer]",
-							path = "[Path]",
-						})[entry.source.name]
-						return vim_item
-					end,
-				},
-				sources = {
+
+				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
-					{ name = "luasnip" },
+				}, {
 					{ name = "buffer" },
-					{ name = "path" },
-				},
+				})
 			})
 		end,
 	},
